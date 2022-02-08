a408fa84fb9e1d6d379edb5893fe370dc1995efc
coder101010 <46952424+coder101010@users.noreply.github.com>
2022-02-08 09:49:53 -0500

    remove commented code

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,14 +30,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
  import Echo from 'laravel-echo';
-
- /* Disable Pusher for now 
- window.Pusher = require('pusher-js');
-
- window.Echo = new Echo({
-     broadcaster: 'pusher',
-     key: process.env.MIX_PUSHER_APP_KEY,
-     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-     forceTLS: false
- });
- */
